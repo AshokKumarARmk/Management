dedc94ba1462e8821d157fc56710e6cdf0481f0a
Ashok Kumar A <ashokkumar071001@gmail.com>
2022-03-29 18:19:39 +0530

    'springboot-student-backend'

---
+++ src/main/java/net/javaguides/springboot/SpringbootStudentBackendApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStudentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStudentBackendApplication.class, args);
+	}
+
+}
